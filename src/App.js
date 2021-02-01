bae31a9ff2676a9c2fb53d382f0db70c57a1089c
Diego <diego35.da@gmail.com>
2021-01-31 20:02:45 -0500

    react router imported and implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      Hi
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/signup.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function signUp() {
+  return (
+    <div>
+      <h1>signup page</h1>
+    </div>
+  );
+}
+export default signUp;
